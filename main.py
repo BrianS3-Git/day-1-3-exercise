fb61968ccaa323c4e79a3fc186b12fa8c7cce945
Brian S <88114504+BrianS3-Git@users.noreply.github.com>
2021-07-28 22:45:55 +0000

    Completed code in one line, 3 functions

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 #Write your code below this line 👇
-print("Hello " +input("What is your name? ")+ "!")
+
+#Look at in from most inside-nested function: input. It in turn feeds the function len, which in turn is processed by function print
+
+print(len(input("What is your name? ")))
+
+
 
 
 
